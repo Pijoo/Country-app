39011cad0171e020f591f31fddc0faa18ee920f8
Pijo <surzyn_b@interia.pl>
2018-02-02 14:18:18 +0100

    task_20.4-counrty-app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import routes from './routes';
 
 
 render(
 		<Provider store={store}>
-				<h1>Inicjalizacja projektu</h1>
+				<Router history={hashHistory} routes={routes}/>
 		</Provider>,
 		document.getElementById('root')
 );
